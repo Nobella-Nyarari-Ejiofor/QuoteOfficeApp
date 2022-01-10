d3d7e544c245d5df5bf4157ca58aca605a7302ce
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-09 22:36:23 -0500

    Made the array for objects

---
+++ src/app/Quote.ts
@@ -0,0 +1,11 @@
+export interface Quote {
+  // [x: string]: any;
+  id : number ;
+  quotetext : string ;
+  author:string ;
+  name:string ;
+  upvote : number ;
+  downvote : number ;
+  date : Date
+
+}
---
+++ src/app/mock-quotes.ts
@@ -0,0 +1,42 @@
+import{Quote} from './Quote'
+ 
+export const QUOTES: Quote[] = [
+  {
+    id: 1 ,
+    quotetext: " When it rains it pours" ,
+    author : "Jane Hamsburg",
+    name : "Kamau Thiongo",
+    upvote: 0 ,
+    downvote : 0 ,
+    date : new Date("2021-12-25")
+       
+    
+  } ,
+  {
+    id:2 ,
+     quotetext: "The early bird catches the worm",
+     author : "Unknown",
+     name : "James Lee",
+     upvote: 0 ,
+     downvote : 0 ,
+     date : new Date("2021-12-25")
+   },
+   {
+    id:3 ,
+    quotetext : "The early bird doesn't  the worm, the prepared bird does!",
+    author : "James wa Thiongo",
+    name : "Agnes Butita",
+    upvote: 0 ,
+    downvote : 0 ,
+    date : new Date("2021-12-25T08:00")
+  },
+  {
+    id:4 ,
+    quotetext : "A smooth sail didn't make a skilled Sailor",
+    author : "Unknown",
+    name : "Maina Wachera",
+    upvote: 0 ,
+    downvote : 0 ,
+    date : new Date("2021-12-25")
+  },
+ ]
