218deb96f9ab83e3c6655f3026a83f7a7af16a13
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-09 23:09:44 -0500

    made changes to the quotes array

--- src/app/mock-quotes.ts
+++ src/app/mock-quotes.ts
@@ -8,21 +8,12 @@ export const QUOTES: Quote[] = [
     name : "Kamau Thiongo",
     upvote: 0 ,
     downvote : 0 ,
-    date : new Date("2021-12-25")
+    date : new Date("2022-1-05")
        
     
   } ,
-  {
-    id:2 ,
-     quotetext: "The early bird catches the worm",
-     author : "Unknown",
-     name : "James Lee",
-     upvote: 0 ,
-     downvote : 0 ,
-     date : new Date("2021-12-25")
-   },
    {
-    id:3 ,
+    id:2 ,
     quotetext : "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking",
     author : "Steve Jobs",
     name : "Agnes Butita",
@@ -31,7 +22,7 @@ export const QUOTES: Quote[] = [
     date : new Date("2017-06-02")
   },
   {
-    id:4 ,
+    id:3 ,
     quotetext : "A smooth sail didn't make a skilled Sailor",
     author : "Unknown",
     name : "Maina Wachera",
