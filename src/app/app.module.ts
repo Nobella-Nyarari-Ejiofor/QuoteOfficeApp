bbae0d7a2fbfb6ab488569234069794c5b487648
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-09 22:46:21 -0500

    Created a pipe for the time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { QuoteItemComponent } from './component/quote-item/quote-item.component';
 import { QuoteFormComponent } from './component/quote-form/quote-form.component';
+import { QuoteItemComponent } from './component/quote-item/quote-item.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './component/footer/footer.component';
+import { TimepipePipe } from './timepipe.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
+    QuoteFormComponent,
+
     QuoteItemComponent,
-    QuoteFormComponent
+    FooterComponent,
+    TimepipePipe,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  
+    HttpClientModule ,
+    FormsModule,
+    
+    
   ],
+  // not sure whether to place the class in providers
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/component/quote-form/quote-form.component.ts
+++ src/app/component/quote-form/quote-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+// import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit , Output,EventEmitter} from '@angular/core';
+import { Quote  } from 'src/app/Quote';
+import { QUOTES } from 'src/app/mock-quotes';
+
+
 
 @Component({
   selector: 'app-quote-form',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+
+  @Output() onAddQuote :EventEmitter<Quote> = new EventEmitter()
+
+  //  Defining the types of data that will be input in our forms
+  quotetext!: string;
+  author!: string ;
+  name! : string ;
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+onSubmit(){
+if(!this.quotetext){
+    alert("Please add a quote make a submission.");
+    return
+  } 
+ const newQuote ={
+   quotetext : this.quotetext ,
+   name : this.name ,
+   author : this.author ,
+   id : this.generateId(),
+   upvote : 0,
+   downvote: 0 ,
+   date: new Date()
+
+ } ;
+
+ this.onAddQuote.emit(newQuote)
+ 
+ //Makes forms empty after add quote button is clicked
+
+ this.quotetext = '',
+ this.name = '',
+ this.author=''
 }
+generateId(){
+ return parseInt( (new Date()).getTime().toString());
+}
+
+ 
+};
