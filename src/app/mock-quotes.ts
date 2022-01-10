9af2d5fae57969da429bc4492c83ea050bf38512
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-09 23:06:51 -0500

    Changed the quote in the quotes

--- src/app/mock-quotes.ts
+++ src/app/mock-quotes.ts
@@ -3,7 +3,7 @@ import{Quote} from './Quote'
 export const QUOTES: Quote[] = [
   {
     id: 1 ,
-    quotetext: " When it rains it pours" ,
+    quotetext: "At the end of the day, your feet should be dirty, your hair messy, and your eyes sparkling.",
     author : "Jane Hamsburg",
     name : "Kamau Thiongo",
     upvote: 0 ,
@@ -23,12 +23,12 @@ export const QUOTES: Quote[] = [
    },
    {
     id:3 ,
-    quotetext : "The early bird doesn't  the worm, the prepared bird does!",
-    author : "James wa Thiongo",
+    quotetext : "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking",
+    author : "Steve Jobs",
     name : "Agnes Butita",
     upvote: 0 ,
     downvote : 0 ,
-    date : new Date("2021-12-25T08:00")
+    date : new Date("2017-06-02")
   },
   {
     id:4 ,
