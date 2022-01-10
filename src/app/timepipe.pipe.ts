7d605ca66561ac7e1fead62b0fc54d05e6b239a9
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-09 22:49:01 -0500

    Added the function to the pipe

--- src/app/timepipe.pipe.ts
+++ src/app/timepipe.pipe.ts
@@ -5,8 +5,39 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimepipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: Date, ...args: unknown[]): unknown {
+    return this.timeDiff(value);
+    
   }
+   timeDiff (date : Date) {
+    var seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return "Just Now"
+   }
+
+ numberEnding (number :number) {
+  return (number > 1) ? 's' : '';
+}
 
 }
